da4a129d9a45e214217258f912b555abbfe4aa07
henoxx <henocktizazu6@gmail.com>
2022-08-28 16:22:10 +0300

    better classmethods

--- predictz.py
+++ predictz.py
@@ -6,6 +6,7 @@ Developed by @Henoxx
 from bs4 import BeautifulSoup
 import requests
 from tabulate import tabulate
+from pprint import pformat
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 
@@ -15,10 +16,7 @@ BUN_URL = "https://www.predictz.com/predictions/germany/bundesliga/"
 MAJOR_LEAGUES = [EPL_URL, LALIGA_URL, BUN_URL]
 
 class Predictz():
-    def __init__(self,fixtures=None, stakes=None, predictions=None, data_dict=None, table_form=None):
-        self.fixtures = fixtures
-        self.stakes = stakes
-        self.predictions = predictions
+    def __init__(self, data_dict=None, table_form=None):
         self.data_dict = data_dict
         self.table_form = table_form
 
@@ -27,12 +25,17 @@ class Predictz():
             return str(tabulate(self.table_form, headers="keys"))
         except:
             return "Some Error occured!"
+    def write_data(self, file_name='data.txt'):
+        file_name = file_name
+        self.fh = open(file_name, 'w')
+        self.fh.write(pformat(self.data_dict))
+        self.fh.close()
+        
+        return print("File saved.")
+
 
     @classmethod
-    def get(cls, url=BUN_URL):
-        if url == 'epl':
-            url = EPL_URL
-        
+    def get(cls, url=LALIGA_URL):
         fixtures = []
         stakes = []
         predictions = []
@@ -42,20 +45,18 @@ class Predictz():
 
         table_rows = soup.find(class_="pztable")
 
-
         for row in table_rows:
             try:
                 fixt = ''.join(list(row.find_all(class_="fixt")[0].a.strings))
                 fixtures.append(fixt)
                 pred = row.contents[0].string
                 stake = pred.split(' ')[0]
-                if stake.lower() != "draw":
-                    stake += " Win"
+                stake = stake if stake.lower() == "draw" else (stake + " Win")
                 stakes.append(stake)
                 predictions.append(pred.split(' ')[1])
                 data_dict[fixt] = {'Stake': stake, 'Prediction': pred.split(' ')[1]}
             except:
                 pass
-
+        
         table_form = {"Fixtures": fixtures, "Stake": stakes, "Predictions":predictions}
-        return cls(fixtures, stakes, predictions, data_dict, table_form)
+        return cls(data_dict, table_form)
